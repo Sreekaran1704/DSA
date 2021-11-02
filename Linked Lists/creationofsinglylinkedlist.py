f59a086956429cda20b38fe67955b607351f53ed
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-02 22:28:29 +0530

    Update creationofsinglylinkedlist.py

--- Linked Lists/creationofsinglylinkedlist.py
+++ Linked Lists/creationofsinglylinkedlist.py
@@ -1,23 +1,37 @@
-class node:
-	def __init__(self,value=None,next=None):
-		self.value=value
-		self.next=next
-class SlinkedList:
-	def __init__(self):
-		self.head=None
-	def createList(self):
-		n=int(input("Enter the size of Linked List : "))
-		for i in range(n):
-			data=int(input())
-			newnode=node(data)
-			if self.head==None:
-				self.head=newnode
-			else:
-				temp=self.head
-				while(temp.next!=None):
-					temp=temp.next
-				temp.next=newnode
-	def printList(self):
-		for i in range(self.n):
-			print()
+# A single node of a singly linked list
+class Node:
+  # constructor
+  def __init__(self, data = None, next=None): 
+    self.data = data
+    self.next = next
 
+# A Linked List class with a single head node
+class LinkedList:
+  def __init__(self):  
+    self.head = None
+  
+  # insertion method for the linked list
+  def createList(self, data):
+    newNode = Node(data)
+    if(self.head!=None):
+      temp = self.head
+      while(temp.next!=None):
+        temp = temp.next
+      temp.next = newNode
+    else:
+      self.head = newNode
+  
+  # print method for the linked list
+  def printLL(self):
+    temp= self.head
+    while(temp!=None):
+      print(temp.data)
+      temp = temp.next
+
+# Singly Linked List with insertion and print methods
+LL = LinkedList()
+for i in range(int(input())):
+	a=int(input())
+	LL.createList(a)
+print("The elements in the list are : ")
+LL.printLL()
