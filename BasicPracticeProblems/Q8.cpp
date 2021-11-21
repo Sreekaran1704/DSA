ea5f855828daf7558e37f01b9300830a54673555
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-21 12:21:09 +0530

    Update Q8.cpp

--- BasicPracticeProblems/Q8.cpp
+++ BasicPracticeProblems/Q8.cpp
@@ -1,1 +1,23 @@
 //Question code : FINDMELI
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    int n, x, i, k;
+    cin >> n >> k;
+ 
+    set<int> s;
+    for (i = 0; i < n; i++)
+    {
+        cin >> x;
+        s.insert(x);
+    }
+ 
+    if (s.find(k) == s.end())
+        cout << -1 << endl;
+    else
+        cout << 1 << endl;
+}
+ 
