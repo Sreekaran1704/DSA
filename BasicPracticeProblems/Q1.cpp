336bd252048ee40be55d8bae3d442de75389f2f7
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-21 11:18:01 +0530

    Added basic problems

---
+++ BasicPracticeProblems/Q1.cpp
@@ -0,0 +1,11 @@
+//Question Code : BUYPLSE
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	cout<<((a*x)+(b*y));
+	return 0;
+}
