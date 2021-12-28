45a407b21d90c400dd311299397f1c8339e39c1c
Sreekaran1704 <86601811+Sreekaran1704@users.noreply.github.com>
2021-12-28 06:55:37 +0530

    Update Quicksort.py

--- Sortings/Quick Sort/Quicksort.py
+++ Sortings/Quick Sort/Quicksort.py
@@ -1,19 +1,22 @@
-def partition(customlist,low,high):
+def partition(arr,low,high):
+    pivot=arr[high]
     i=low-1
-    pivot=customlist[high]
+
     for j in range(low,high):
-        if customlist[j]<=pivot:
+        if arr[j]<=pivot:
             i+=1
-            customlist[i],customlist[j]=customlist[j],customlist[i]
-        else:
-            customlist[i+1],customlist[high]=customlist[high],customlist[i+1]
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
     return i+1
-def quicksort(customlist,low,high):
+
+
+def quick_sort(arr,low,high):
     if low<high:
-        pi=partition(customlist,low,high)
-        quicksort(customlist,low,pi-1)
-        quicksort(customlist,pi+1,high)
-customlist=list(map(int,input().split()))
-high=len(customlist)-1
-(quicksort(customlist,0,high))
-print(customlist)
+        pi=partition(arr,low,high)
+
+        quick_sort(arr,low,pi-1)
+        quick_sort(arr,pi+1,high)
+
+arr=list(map(int,input().split()))
+quick_sort(arr,0,len(arr)-1)
+print(arr)
