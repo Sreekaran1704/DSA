2123ea50bd52488e28ffb129992197a1436017bc
Sreekaran1704 <86601811+Sreekaran1704@users.noreply.github.com>
2022-02-13 16:03:46 +0530

    commit to main

--- Sortings/Bubble sort/Bubblesort.py
+++ Sortings/Bubble sort/Bubblesort.py
@@ -2,6 +2,6 @@ size = int(input("Enter the size of the array: "))
 l = list(map(int,input().split()))
 for i in range(size-1):
     for j in range(size-1):
-        if l[j+1]<l[j]:
-            l[j+1],l[j]=l[j],l[j+1]
+        if l[i]<l[j]:
+            l[i],l[j]=l[j],l[i]
 print(l)
